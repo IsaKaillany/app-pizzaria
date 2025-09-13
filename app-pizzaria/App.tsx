5dc051bce01fec4e1303bd7876deaeae9954062c
Isa Kaillany <isakaillany123@gmail.com>
2025-09-13 17:02:41 -0300

    init project

---
+++ app-pizzaria/App.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import SignIn from "./src/pages/SignIn";
+
+export default function App() {
+    return (
+        <View>
+            <SignIn />
+        </View>
+    );
+}
